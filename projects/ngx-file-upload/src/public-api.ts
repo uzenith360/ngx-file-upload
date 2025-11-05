51298171f92047626b21c836c5d41672b8265535
Zenith Wogwugwu <uzenith360@gmail.com>
2025-11-05 04:28:42 +0100

    fix: add file upload progress UI

--- projects/ngx-file-upload/src/public-api.ts
+++ projects/ngx-file-upload/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of ngx-file-upload
  */
 
-export * from './lib/file-uploads-progress/file-uploads-progress.component';
 export * from './lib/file-upload/file-upload-response.interface';
 export * from './lib/file-upload/file-upload-timeout.error';
 export * from './lib/file-upload/file-upload.service';
